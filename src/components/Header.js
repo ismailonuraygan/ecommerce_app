6b46c914d7a3908562c4ae1ea3aff561e0def6a3
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-09-23 21:46:12 +0300

    Building search bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import Image from "next/image";
-import logo from '../public/image/amazonLogo.png';
+import logo from '../public/image/download-removebg-preview.png';
+import {MenuIcon, SearchIcon , ShoppingCartIcon} from '@heroicons/react/outline';
 
 function Header() {
     return (
         <header>
-            <div>
-                <div>
+            <div className="flex items-center bg-yellow-300 p-1 flex-grow">
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
                     <Image
                          src={logo}
-                        width={150}
-                        height={40}
+                        width={100}
+                        height={70}
                         objectFit="contain"
                         className="cursor-pointer"
                     />
+                </div>
+                {/* Search */}
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-gray-400 hover:bg-gray-500">
+                    <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4" type="text" />
+                    <SearchIcon className="h-12 p-4"/>
                 </div>
             </div>
             <div>
