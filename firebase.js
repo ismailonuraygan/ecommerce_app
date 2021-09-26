c5ec71041c911efc001d75045585a43a8f130e48
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-09-26 15:34:00 +0300

    Installed firebase dependency andset firebase config

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBGK0unkL0SPs8D35pw-wEyWnq6AKcMFPI",
+    authDomain: "mustache-store.firebaseapp.com",
+    projectId: "mustache-store",
+    storageBucket: "mustache-store.appspot.com",
+    messagingSenderId: "285923085354",
+    appId: "1:285923085354:web:f2c325ea1f0e2e98f40753",
+    measurementId: "G-2LJLEF7FGR"
+  };
