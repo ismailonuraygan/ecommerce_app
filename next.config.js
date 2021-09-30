04552a61550ea6bb825fbd2586cd27e07ab8cb8f
İsmail Onur AYGAN <72319363+ismailonuraygan@users.noreply.github.com>
2021-10-01 00:27:54 +0300

    Adding domains

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ["https://links.papareact.com", "fakestoreapi.com"],
+  },
+  resolve: {
+    fallback: {
+        "fs": false
+    },
+}
 }
